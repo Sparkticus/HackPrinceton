4af4c964f1f8fb055a16712cc827cc22cc67c6da
Surbhi Madan <surbhi_madan@brown.edu>
2014-03-29 22:31:49 -0400

    Front end pages

---
+++ input.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('#add').click(function(e) {
+		e.preventDefault();
+		
+		var newInput = document.createElement("input");
+		newInput.type="text";
+		newInput.name="Field";
+		$(newInput).attr("id","field");
+		document.getElementById('recipe').appendChild(newInput);
+
+		var uploadImage = document.createElement("input");
+		uploadImage.type="file";
+		uploadImage.name="image";
+		$(uploadImage).attr("id","image");
+		document.getElementById('recipe').appendChild(uploadImage);
+
+	});
+
+});
